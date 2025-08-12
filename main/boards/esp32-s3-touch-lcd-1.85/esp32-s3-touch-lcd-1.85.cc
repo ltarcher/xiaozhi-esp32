787d4170ea470145ee299bffebf7c4f2101f1bb5
ltarcher <59073821@qq.com>
2025-08-12 19:55:30 +0800

    refactor: 为ESP32-S3-Touch-LCD-1.85添加I2C总线配置，启用内部上拉并设置毛刺过滤

--- main/boards/esp32-s3-touch-lcd-1.85/esp32-s3-touch-lcd-1.85.cc
+++ main/boards/esp32-s3-touch-lcd-1.85/esp32-s3-touch-lcd-1.85.cc
@@ -399,6 +399,10 @@ private:
             .sda_io_num = TP_PIN_NUM_SDA,
             .scl_io_num = TP_PIN_NUM_SCL,
             .clk_source = I2C_CLK_SRC_DEFAULT,
+            .glitch_ignore_cnt = 7,
+            .flags = {
+                .enable_internal_pullup = 1,
+            },
         };
         ESP_ERROR_CHECK(i2c_new_master_bus(&i2c_bus_cfg, &tp_i2c_bus));
         
